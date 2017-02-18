5bd138de3bac3c6cbbca725c4bfe62629b768025
Ben Lancaster <bdl@live.co.uk>
2017-02-18 18:45:31 +0000

    board: Dynamically create columns for bug statuses. Extra information displayed.
    Signed-off-by: Ben Lancaster <bdl@live.co.uk>

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -3,13 +3,23 @@
 var bug_list = [];
 var board_sections = [];
 
-function make_bug_item(id, sect, title, desc, assignee, component) {
+function make_bug_item(id
+                       ,product
+                       ,component
+                       ,assignee
+                       ,status
+                       ,res
+                       ,desc
+                       ,changed)
+{
   this.id = id;
-  this.sect = sect;
-  this.title = title;
-  this.desc = desc;
-  this.assignee = assignee;
+  this.product = product;
   this.component = component;
+  this.assignee = assignee;
+  this.status = status;
+  this.res = res;
+  this.desc = desc;
+  this.changed = changed;
 
   return this;
 }
@@ -25,35 +35,42 @@ window.addEventListener("load", function() {
   viewport.setAttribute('ng-controller', 'MainController');
   app.controller('MainController', function ($scope) {
     $scope.bug_list = bug_list;
+    $scope.board_sections = board_sections;
   });
 
   // Loop through bugs list bz_buglist
   $(".bz_buglist").find(".bz_bugitem").each(function() {
     console.log(this);
-    var b_id = $(this).first().find("a").first().text();
-    var b_status = $(this).find(".bz_bug_status_column").first().text();
-    var b_title = $(this).find(".bz_short_desc_column").first().text();
-    var b_assignee = $(this).find(".bz_assigned_to_column").first().text();
-    var b_component = $(this).find(".bz_component_column").first().text();
-    console.log(b_id + " " + b_status + " " + b_title);
+    var b_id =        $(this).first().find("a").first().text().trim();
+    var b_product =   $(this).find(".bz_product_column").first().text().trim();
+    var b_component = $(this).find(".bz_component_column").first().text().trim();
+    var b_assignee =  $(this).find(".bz_assigned_to_column").first().text().trim();
+    var b_status =    $(this).find(".bz_bug_status_column").first().text().trim();
+    var b_res =       $(this).find(".bz_resolution_column").first().text().trim();
+    var b_desc =      $(this).find(".bz_short_desc_column").first().text().trim();
+    var b_changed =   $(this).find(".bz_changeddate_column").first().text().trim();
 
     var bug_item = new make_bug_item(
-      b_id,
-      b_status,
-      b_title,
-      '',
-      b_assignee,
-      b_component
+      b_id
+      ,b_product
+      ,b_component
+      ,b_assignee
+      ,b_status
+      ,b_res
+      ,b_desc
+      ,b_changed
     );
+    console.log(bug_item);
 
     bug_list.push(bug_item);
 
-    if($.inArray(b_status, board_sections) >= 0) {
+    if(board_sections.indexOf(b_status)  == -1) {
       board_sections.push(b_status);
     }
   });
   console.log("Number of bugs: " + bug_list.length);
   console.log(board_sections);
+  console.log(bug_list);
 
   var board = document.createElement('div');
   board.setAttribute('board', '');
